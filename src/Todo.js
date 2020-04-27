5af80f36eda6b67dd0b6a0e37d3da30cf2cc58da
KSentak <k.sentak@outlook.com>
2020-04-27 00:20:02 -0400

    began CRUD functionality in Todo component

---
+++ src/Todo.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import Checkbox from '@material-ui/core/Checkbox';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import IconButton from '@material-ui/core/IconButton';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+
+function Todo({ task, completed }) {
+  return (
+    <ListItem>
+      <Checkbox tabIndex={-1} checked={completed} />
+      <ListItemText
+        style={{ textDecoration: completed ? 'line-through' : 'none' }}
+      >
+        {task}
+      </ListItemText>
+      <ListItemSecondaryAction>
+        <IconButton>
+          <DeleteIcon aria-label='Delete' />
+        </IconButton>
+        <IconButton>
+          <EditIcon aria-label='Edit' />
+        </IconButton>
+      </ListItemSecondaryAction>
+    </ListItem>
+  );
+}
+
+export default Todo;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,8 @@
 import React from 'react';
+import Todo from './Todo';
 
 import Divider from '@material-ui/core/Divider';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 import Paper from '@material-ui/core/Paper';
 
 function TodoList(props) {
@@ -12,9 +11,7 @@ function TodoList(props) {
       <List>
         {props.todos.map((todo) => (
           <>
-            <ListItem>
-              <ListItemText>{todo.task}</ListItemText>
-            </ListItem>
+            <Todo task={todo.task} key={todo.id} completed={todo.completed} />
             <Divider />
           </>
         ))}
